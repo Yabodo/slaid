31db13d9fe5a312d006d4d90b0d8e5c512fd8199
Kaspar Jõeveer <kjoeveer@gmail.com>
2022-10-19 22:05:54 +0300

    Production ready

---
+++ components/SlaidLogo.vue
@@ -0,0 +1,6 @@
+<template>
+  <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+    <img src="favicon.ico" alt="" class="w-8 h-8">
+    <span class="ml-3 text-xl">Slaid.ee</span>
+  </a>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,309 @@
+
+
+<script setup lang="ts">
+  const client = useSupabaseClient()
+  async function createOrder() {
+    const { data, error } = await client
+      .from("orders")
+      .insert([{ 
+        source: "website", 
+        name: customer.value.name,
+        phone: customer.value.phone,
+        email: customer.value.email,
+        reference: paymentReference
+      }]);
+    if (error) return error;
+    return { id: data[0].id, error };
+  }
+
+    /* ref() and computed() are auto-imported */
+    const date = ref(new Date().getFullYear())
+    const randomString = Math.random().toString(36).slice(3, 9);
+    const randomTime = Math.random()*1000
+    const paymentReference = (randomString+randomTime).replaceAll(".","").slice(1, 10)
+
+    const notification = ref({
+      tag: "Oi?!",
+      description: "Veendu, et kõik sisestatud andmed oleksid korrektsed.",
+      ref: "#order",
+      show: false
+    })
+
+    let products = ref([
+      {
+        name: "Kaks parimat",
+        description: "2 eset*",
+        quantity: 2,
+        price: 30,
+        selected: false
+      },
+      {
+        name: "Kümme parimat",
+        description: "10 eset*",
+        quantity: 10,
+        price: 135,
+        selected: false
+      },
+      {
+        name: "Riiulitäis",
+        description: "20 eset*",
+        quantity: 20,
+        price: 250,
+        selected: false
+      },
+      {
+        name: "Kogu arhiiv",
+        description: "40 eset*",
+        quantity: 40,
+        price: 500,
+        selected: false
+      },
+      {
+        name: "Konsultatsioon",
+        description: "kuni 15min",
+        quantity: 1,
+        price: 0,
+        selected: false
+      }
+    ])
+    const cart = ref([])
+    const cartSum = computed(() => {
+      let sum = 0;
+      cart.value.forEach(item => {
+        sum += item.price
+      });
+      return sum
+    })
+    const customer = ref({
+      name: "",
+      email: "",
+      phone: ""
+    })
+
+    function sleep (time) {
+      return new Promise((resolve) => setTimeout(resolve, time));
+    }
+
+    function sendNotification(description:string) {
+      notification.value.description = description;
+      notification.value.show = true;
+      sleep(2900).then(() => {
+        notification.value.show = false;
+      })
+    }
+
+    function placeOrder() {
+      if(cart.value.length < 1) return sendNotification("Tundub, et teie ostukorv on tühi.");
+      if(customer.value.email.length < 10) return sendNotification("Tundub, et teie e-posti aadress on vigane.");
+      if(customer.value.name.length < 1) return sendNotification("Nimi on kohustuslik väli, et teaksime kellega edaspidi vestleme.");
+      if(customer.value.phone.length < 7) return sendNotification("Tundub, et teie telefoninumber on vigane.");
+      if(cartSum.value > 0) {
+        createOrder();
+        let url = `https://payment.test.maksekeskus.ee/pay/1/link.html?shop=f7741ab2-7445-45f9-9af4-0d0408ef1e4c&amount=${ cartSum.value }&reference=${ paymentReference }&country=ee&locale=et`;
+        window.open(url);
+      }
+    }
+</script>
+
+<template>
+  <div>
+    <div v-show="notification.show" class="text-center py-4 lg:px-4 fixed translate-y-1/2 -translate-x-1/2 left-1/2">
+      <div class="p-2 bg-red-800 items-center text-red-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+        <span class="flex rounded-full bg-red-500 uppercase px-2 py-1 text-xs font-bold mr-3">{{ notification.tag }}</span>
+        <span class="font-semibold mr-2 text-left flex-auto">{{ notification.description }}</span>
+        <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+      </div>
+    </div>
+    <header class="text-gray-600 body-font">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <SlaidLogo />
+    <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+      <a class="mr-5 hover:text-gray-900 cursor-pointer">Digitaliseerimine</a>
+      <a class="mr-5 hover:text-gray-900 cursor-pointer">Arhiveerimine</a>
+      <a class="mr-5 hover:text-gray-900 cursor-pointer">Kontakt</a>
+    </nav>
+    <a href="#order">
+      <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+        <p>Tellima</p>
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1 mt-1" viewBox="0 0 24 24" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke:currentColor;">
+          <path d="M5 12h14M12 5l7 7-7 7"></path>
+        </svg>
+      </button>
+    </a>
+  </div>
+</header>
+
+
+  <section class="text-gray-600 body-font">
+  <div class="container mx-auto flex px-5 py-6 md:py-24 md:flex-row flex-col-reverse items-center">
+    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Päästame oma
+        <br class="hidden lg:inline-block">tuhmuvad mälestused
+      </h1>
+      <p class="mb-8 leading-relaxed">Kas teadsid, et VHS kassettide keskmine eluiga jääb etc Banh mi cornhole echo park skateboard authentic crucifix neutra tilde lyft biodiesel artisan direct trade mumblecore 3 wolf moon twee Banh mi cornhole echo park skateboard authentic crucifix neutra tilde lyft biodiesel artisan direct trade mumblecore 3 wolf moon twee</p>
+      <div class="flex justify-center">
+        <a href="#order">
+          <button  class="flex ml-auto h-fit text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+            <p>Tellima</p>
+          </button>
+        </a>
+        <button class="ml-4 flex h-fit text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded">
+          <p>Kliendid</p>
+        </button>
+      </div>
+    </div>
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <img class="object-cover object-center rounded" alt="hero" :src="`assets/img/${'hero'}.png`">
+    </div>
+  </div>
+</section>
+
+<section id="order" class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-10">
+      <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Hinnakiri</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Teame, et Teie mälestused on teie jaoks hindamatud, mis tõttu kasutame me oma töös parimaid võimalikke töövahendeid ja tehnoloogiaid (nt. eelpuhastus ja topeltrenderdus). Meie eesmärk ja südamesoov on, et teie mälestused jääksid teid alatiseks saatma!</p>
+    </div>
+    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+      <table class="table-auto w-full text-left whitespace-no-wrap">
+        <thead>
+          <tr>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">Pakett</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Kogus</th>
+            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+              Hind €
+            </th>
+            <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, i) in products" :key="i">
+            <td class="border-t-2 border-gray-200 px-4 py-3">{{ item.name }}</td>
+            <td class="border-t-2 border-gray-200 px-4 py-3">{{ item.description }}</td>
+            <td v-if="item.price == 0" class="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900"><p style="font-size: 0.95rem;">TASUTA</p></td>
+            <td v-else class="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900"><p style="font-size: 0.95rem;">{{ item.price }}</p></td>
+            <td class="border-t-2 border-gray-200 w-10 text-center">
+              <input :id="item.name" type="checkbox" v-model="cart" :value="item">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="pt-2">
+      <div v-if="cart.length > 0" class="relative lg:w-2/3 w-full mx-auto">
+        <label for="email" class="leading-7 text-sm text-gray-500">E-posti aadress</label>
+        <input required v-model="customer.email" type="email" id="email" name="email" class="w-full bg-gray-100 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-800 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+      </div>
+      <div v-if="customer.email.length > 0 && cart.length > 0" class="flex gap-4 relative lg:w-2/3 w-full mx-auto">
+        <div class="w-1/2">
+          <label for="name" class="leading-7 text-sm text-gray-500">Nimi</label>
+          <input required v-model="customer.name" type="text" id="name" name="name" class="w-full bg-gray-100 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-800 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+        </div>
+        <div class="w-1/2">
+          <label for="phone" class="leading-7 text-sm text-gray-500">Telefoninumber</label>
+          <input required v-model="customer.phone" type="text" id="phone" name="phone" class="w-full bg-gray-100 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-800 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+        </div>
+      </div>
+    </div>
+    <div class="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
+      <div>
+        <p>* ese ehk 1x videokassett, 2x audiokassett või 20x pilt</p>
+        <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Loe lisaks
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke:currentColor;">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+      <button @click="placeOrder()" class="flex ml-auto h-fit text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+        <p>Telli</p>
+      </button>
+    </div>
+  </div>
+</section>
+
+<section id="testimonials" class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <h1 class="text-3xl font-medium title-font text-gray-900 mb-12 text-center">Klientide tagasiside</h1>
+    <div class="flex flex-wrap -m-4">
+      <div class="p-4 md:w-1/2 w-full">
+        <div class="h-full bg-gray-100 p-8 rounded">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;">
+            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+          </svg>
+          <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
+          <a class="inline-flex items-center">
+            <img alt="testimonial" :src="`assets/img/${'Merli-156x156'}.png`" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
+            <span class="flex-grow flex flex-col pl-4">
+              <span class="title-font font-medium text-gray-900">Merli Mirk</span>
+              <span class="text-gray-500 text-sm">Vaimne tervendaja</span>
+            </span>
+          </a>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/2 w-full">
+        <div class="h-full bg-gray-100 p-8 rounded">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;">
+            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+          </svg>
+          <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
+          <a class="inline-flex items-center">
+            <img alt="testimonial" :src="`assets/img/${'Alex-156x156'}.png`" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
+            <span class="flex-grow flex flex-col pl-4">
+              <span class="title-font font-medium text-gray-900">Alex Aus</span>
+              <span class="text-gray-500 text-sm">Ettevõtja & suunamudija</span>
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
+      <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">Veendu, et väärt mälestused saavad talletatud, sest noorus ei tule iial tagasi.</h1>
+      <a href="#order">
+        <button class="flex ml-auto h-fit text-white bg-indigo-500 border-0 mt-6 sm:mt-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+          <p>Tellima</p>
+        </button>
+      </a>
+    </div>
+  </div>
+</section>
+
+<footer class="text-gray-600 body-font">
+  <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+    <SlaidLogo />
+    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© {{ date }} Kaspar Jõeveer —
+      <a href="https://linkedin.com/in/bizi" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@bizi</a>
+    </p>
+    <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+      <a class="text-gray-500">
+        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;">
+          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500">
+        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;">
+          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500">
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke:currentColor;">
+          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-500">
+        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24" data-darkreader-inline-fill="" data-darkreader-inline-stroke="" style="--darkreader-inline-fill:currentColor; --darkreader-inline-stroke:currentColor;">
+          <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke:none;"></path>
+          <circle cx="4" cy="4" r="2" stroke="none" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke:none;"></circle>
+        </svg>
+      </a>
+    </span>
+  </div>
+</footer>
+
+</div>
+</template>
